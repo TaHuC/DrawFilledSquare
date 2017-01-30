c50ca3218121a456aa3a0ab1eb82113c244e1780
TaHuC <TaHuC@hotmail.com>
2017-01-30 09:06:57 +0200

    Finish this

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace DrawFilledSquare
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			int n = int.Parse(Console.ReadLine());
+
+			PrintRow(n);
+			for (int i = 0; i < n - 2; i++)
+			{
+				PrintMiddleRow(n);
+			}
+			PrintRow(n);
+		}
+
+		public static void PrintRow(int n)
+		{
+			Console.WriteLine(new string('-', 2 * n));
+		}
+
+		public static void PrintMiddleRow(int n)
+		{
+			Console.Write('-');
+			for (int i = 1; i < n ; i++)
+			{
+				Console.Write("\\/");
+			}
+			Console.WriteLine('-');
+		}
+	}
+}
